7ab398e6428c61d0add805dd8f2fa9d5acf56fc6
leonardoserra <serra.leonardo92@gmail.com>
2023-10-02 11:47:22 +0200

    aggiunti ErrorMessage su Pizza model e altre dataAnnotations

--- la-mia-pizzeria-crud-mvc/Models/Pizza.cs
+++ la-mia-pizzeria-crud-mvc/Models/Pizza.cs
@@ -10,22 +10,26 @@ namespace la_mia_pizzeria_crud.Models
         [Column("id")]
         public int Id { get; set; }
         
-        [Required]
+        [Required(ErrorMessage ="Nome obbligatorio")]
+        [StringLength(50,ErrorMessage ="Massimo 50 caratteri")]
         [Column("name")]
         public string Name { get; set; }
 
+        [StringLength(500, ErrorMessage = "Massimo 500 caratteri")]
         [Column("description")]
         [DefaultValue("")]
         public string? Description { get; set; }
 
         [Column("price")]
+        [Range(0.5,50,ErrorMessage ="Il prezzo di una pizza puo' variare da 0.5 a 50€")]
         public float? Price { get; set; }
+
+        [Url(ErrorMessage = "Inserisci un url valido")]
         [Column("image_path")]
         [DefaultValue("default.png")]
-
         public string? ImagePath { get; set; }
 
-
+        //constructors
         public Pizza() { }
         public Pizza(int id, string name, string? description, float? price, string? imagePath)
         {

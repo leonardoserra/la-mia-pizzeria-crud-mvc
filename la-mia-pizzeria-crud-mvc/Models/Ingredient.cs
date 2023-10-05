e8e855a1de3f8468b12d9fd03355fddba025b51d
leonardoserra <serra.leonardo92@gmail.com>
2023-10-05 12:11:21 +0200

    creato model Ingredient e relazionato con Pizza

---
+++ la-mia-pizzeria-crud-mvc/Models/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_crud.Models
+{
+    public class Ingredient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Il nome dell'ingrediente é obbligatorio")]
+        [StringLength(30, ErrorMessage ="Massimo 30 caratteri")]
+        public string Name { get; set; }
+
+        public List<Pizza> pizzas { get; set; }
+    }
+}
